4c7753323c73d57d5b6723531e30327591cb7efc
leoonardo887 <Leonardoborowsky2010@gmail.com>
2026-06-30 22:31:53 -0300

    pitagoras

--- funcões/bhaskara.py
+++ funcões/bhaskara.py
@@ -2,10 +2,12 @@ def bhaskara(a, b, c):
     delta = b ** 2 - 4 * a * c
     bhaskara1 = (-b + delta ** 0.5) / (2 * a)
     bhaskara2 = (-b - delta ** 0.5) / (2 * a)
+    return bhaskara1, bhaskara2
 
 a = float(input("Digite o valor do número que acompanha o x²: "))
 b = float(input("Digite o valor do número que acompanha o x: "))
 c = float(input("Digite o valor do número independente: "))
 
+bhaskara1, bhaskara2 = bhaskara(a, b, c)
 
-print(bhaskara(a, b, c))
+print("O primeiro x vale", bhaskara1, "e o segundo x vale", bhaskara2)
