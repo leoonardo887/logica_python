cf56a81b25f865bb9910dc9e1ec1df6f1165ce2e
porciuncula <joao.v.menezes24@aluno.senai.br>
2026-07-22 21:48:28 -0300

    upd

--- arquivos/write.py
+++ arquivos/write.py
@@ -1,3 +1,7 @@
 with open("saida.txt", "w") as f:
     f.write("Olá, mundo!")
     f.write("Segunda linha.")
+
+
+            #importante, a diferença de 'a' e 'w' é que 'a' serve de append, ou seja, adiciona algo ao arquivo.
+            #Já o 'w' de write, exclue tudo que já tem no arquivo e adiciona algo escrito. 
