e834928d78fef2bbcef6b2fcfc1af8c586e32eba
leoonardo887 <Leonardoborowsky2010@gmail.com>
2026-08-28 22:11:09 -0300

    casa tkinter

--- tkinter/canvas/casa.py
+++ tkinter/canvas/casa.py
@@ -3,7 +3,7 @@ from tkinter import Tk, Canvas
 janela = Tk()
 janela.geometry("500x400")
 
-canvas= Canvas(janela, width=400, height=300, bg="yellow")
+canvas= Canvas(janela, width=400, height=300, bg="light blue")
 
 canvas.create_rectangle(
     250, 200, 100, 100,
@@ -36,6 +36,31 @@ canvas.create_rectangle(
     fill = 'yellow',
     outline='black'
 )
+
+canvas.create_polygon(
+    0,300,
+    0,200,
+    500,200,
+    500,300,
+    fill='green',
+    outline="black"
+)
+
+#carcaça do carro
+canvas.create_polygon(
+    350,160,
+    300,160,
+    280,180,
+    260,190,
+    370,190,
+    fill='blue',
+    outline="black"
+)
+
+#rodas
+canvas.create_oval(305, 200, 280,185, fill='black')
+
+canvas.create_oval(355, 200, 330,185, fill='black')
 
 canvas.pack()
 
